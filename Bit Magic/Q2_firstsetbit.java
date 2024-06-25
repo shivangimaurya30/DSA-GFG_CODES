292aec2289438d782d5391441f2f7074e3931bce
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-06-25 15:58:09 +0530

    Add files via upload

---
+++ Bit Magic/Q2_firstsetbit.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//taking testcases
+		while(t-->0){
+			int n=sc.nextInt();//input n
+			Solution ob=new Solution();
+			System.out.println(ob.getFirstSetBit(n));//calling method
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static int getFirstSetBit(int n){
+            
+        // Your code here
+        if (n == 0) return 0; 
+        
+        int position = 1;  
+        while ((n & 1) == 0) {  
+            n >>= 1; 
+            position++; 
+        }
+        return position; 
+            
+    }
+}

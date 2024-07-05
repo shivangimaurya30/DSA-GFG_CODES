ff7314f80b5be9db27296efd090f92ed79511a51
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-07-05 15:04:33 +0530

    Update countdigitQ1.java

--- countdigitQ1.java
+++ countdigitQ1.java
@@ -1,7 +1,8 @@
 import java.util.*;
 import java.util.Scanner.*;
-class countdigit{
+class solution{
     static int countDigit(int n){
+        //your code here!
         int i=0;
         while (n>0) {
             n=n/10;
@@ -13,6 +14,6 @@ class countdigit{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n=sc.nextInt();
-        System.out.println("Number of digits : "+countDigit(n));
+        System.out.println("Number of digits : "+solution(n));
     }
 }

eceb6efdc9cec4ae5e6e1b8784f183f7cd94c5da
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-07-08 16:50:51 +0530

    Update Q7_PowerSet.java

---
+++ Recursion/Q7_PowerSet.java
@@ -0,0 +1,23 @@
+
+class LexSort
+{
+    //Function to return the lexicographically sorted power-set of the string.
+    static ArrayList<String> powerSet(String s)
+    {
+        // add your code here
+         static ArrayList<String> powerSet(String s)
+    {
+        // add your code here
+         ArrayList<String> result = new ArrayList<>();
+        generatePowSet(s, 0, "", result);
+        return result;
+    }
+    private static void generatePowSet(String s, int index, String current, ArrayList<String> result) {
+        if (index == s.length()) {
+            result.add(current);
+            return;
+        }
+        generatePowSet(s, index + 1, current + s.charAt(index), result); 
+        generatePowSet(s, index + 1, current, result);
+    }
+}

79cdf1eb777e83664e04245c1b5aaca9fa4fe59c
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-07-10 20:51:45 +0530

    Update Q9_MaxANDvalue.java

--- Bit Magic/Q9_MaxANDvalue.java
+++ Bit Magic/Q9_MaxANDvalue.java
@@ -1,5 +1,20 @@
-Class Solution{
-//         // Your code here  
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+//   // Function for finding maximum AND value.
+//     public static int maxAND (int arr[], int N) {
+        
+//         // Your code here
+//         // You can add extra function (if required)
+        
         static int checkBit(int pattern, int arr[], int n)
     {
         int count = 0;
@@ -33,3 +48,32 @@ Class Solution{
     }
     
 }
+
+//{ Driver Code Starts.
+
+class BitWise {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int arr[] = new int[n];
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i]=Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxAND() method of class AND
+		    System.out.println(obj.maxAND(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends

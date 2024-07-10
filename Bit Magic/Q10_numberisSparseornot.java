de4b4099fb01c943ebf5b5c719f50b9ac974bdff
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-07-10 20:28:08 +0530

    Update Q10_numberisSparseornot.java

---
+++ Bit Magic/Q10_numberisSparseornot.java
@@ -0,0 +1,10 @@
+class Solution
+{
+    //Function to check if the number is sparse or not.
+    public static boolean isSparse(int n)
+    {
+        // Your code here
+        return (n & (n >> 1)) == 0;
+    }
+    
+}

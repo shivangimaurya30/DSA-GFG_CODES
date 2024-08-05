0f4ffc7145e93d0e6b2546a281852314b42e3da2
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-08-05 15:36:10 +0530

    Create Q3_FloorinaSortedArray.java

---
+++ Searching/Q3_FloorinaSortedArray.java
@@ -0,0 +1,27 @@
+
+
+class Solution {
+
+    // Function to find floor of x
+    // arr: input array
+    // n is the size of array
+    static int findFloor(long arr[], int n, long x) {
+        int low = 0, high = n - 1;
+        int floor = -1;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(arr[mid] == x) {
+                return mid;
+            } else if(arr[mid] < x) {
+                floor = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return floor;
+    }
+}

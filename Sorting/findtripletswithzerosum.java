31042add07b1b6fbe2aca90c8adbe55ea74823e4
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-09-03 22:04:43 +0530

    Create findtripletswithzerosum.java

---
+++ Sorting/findtripletswithzerosum.java
@@ -0,0 +1,31 @@
+
+
+/*Complete the function below*/
+
+
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+	public boolean findTriplets(int arr[] , int n)
+    {
+        //add code here.
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                
+                if (sum == 0) return true;
+                if (sum < 0) j++;
+                else k--;
+            }
+        }
+        
+        return false;
+    }
+}

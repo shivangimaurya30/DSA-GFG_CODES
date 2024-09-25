bd30b40429b87289c0591726d02965a9ea6a0b3b
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2024-09-25 22:42:45 +0530

    Create transposematrix.java

---
+++ Matrix/transposematrix.java
@@ -0,0 +1,16 @@
+
+
+//User function Template for Java
+class Solution
+{
+    public void transpose(int n,int a[][])
+    {
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+            {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+    }
+}
